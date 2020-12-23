6a4be11d10733122f33887e9c1e608a041886895
daniel-j-dev <danielaugustusjohnson@gmail.com>
2020-12-23 08:29:30 -0500

    Made search bar and added magnifying glass icon

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import './Search.css'
+import React from "react"
+import mg from '../../assets/mg-edit.png'
+import "./Search.css"
 
 const Search = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h4>Location</h4>
+      <div id="search-bar">
+        <img src={mg} />
+        <input />
+      </div>
+    </div>
+  )
 }
 
 export default Search
