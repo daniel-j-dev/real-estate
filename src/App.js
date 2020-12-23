5527cc3c23d926519cc30240b01432413c59bc2e
daniel-j-dev <danielaugustusjohnson@gmail.com>
2020-12-23 10:45:39 -0500

    Added rc-slider and implemented Context API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import { useState } from "react"
+import AppContext from "./contexts/AppContext"
+import "./App.css"
+
 import Header from "./components/header/Header"
 import Search from "./components/search/Search"
 import PropertyType from "./components/propertyType/PropertyType"
 import PriceRange from "./components/priceRange/PriceRange"
 import Results from "./components/results/Results"
-import "./App.css"
 
 function App() {
+  const [listings, setListings] = useState([])
+
+  const [settings] = useState({
+    priceRange: {
+      min: 846,
+      max: 1392,
+      dLeftVal: 954, //Default left slider handle value
+      dRightVal: 1200, //Default right slider handle value
+    },
+  })
+
   return (
     <div className="App">
-      <div id="upper">
-        <Header />
-        <Search />
-        <PropertyType />
-        <PriceRange />
-      </div>
-      <Results />
+      <AppContext.Provider value={{ settings, listings, setListings }}>
+        <div id="upper">
+          <Header />
+          <Search />
+          <PropertyType />
+          <PriceRange />
+        </div>
+        <Results />
+      </AppContext.Provider>
     </div>
   )
 }
--- src/components/priceRange/PriceRange.js
+++ src/components/priceRange/PriceRange.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import './PriceRange.css'
+import React, { useContext } from "react"
+import AppContext from "../../contexts/AppContext"
+import Slider, { createSliderWithTooltip } from "rc-slider"
+import "rc-slider/assets/index.css"
+import "./PriceRange.css"
 
 const PriceRange = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const { settings } = useContext(AppContext)
+
+  const { min, max, dLeftVal, dRightVal } = settings.priceRange
+
+  const Range = createSliderWithTooltip(Slider.Range)
+
+  const handleChanges = (value) => {}
+
+  return (
+    <div id="price-range-container">
+      <h4>Price range</h4>
+      <Range
+        allowCross={false}
+        min={min}
+        max={max}
+        defaultValue={[dLeftVal, dRightVal]}
+        tipFormatter={(value) => `$${value.toLocaleString()}`}
+        onChange={(value) => handleChanges(value)}
+      />
+    </div>
+  )
 }
 
 export default PriceRange
